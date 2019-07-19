b7809a7d2893aeaf9b1e926716694b8453ef4dc1
shankhadeep <shankhadeep.it@gmail.com>
2019-07-19 09:32:47 +0530

    Test cases added

---
+++ src/test/java/com/connect/proj/processor/ProcessDataServiceTest.java
@@ -0,0 +1,27 @@
+package com.connect.proj.processor;
+
+
+import org.junit.Test;
+
+public class ProcessDataServiceTest {
+
+    @Test
+    public void addIntervals() {
+    }
+
+    @Test
+    public void removeIntervals() {
+    }
+
+    @Test
+    public void checkIntervalAndRemove() {
+    }
+
+    @Test
+    public void getExactPreviousInterval() {
+    }
+
+    @Test
+    public void mergeTwoIntervals() {
+    }
+}
